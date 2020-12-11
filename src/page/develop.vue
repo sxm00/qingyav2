dedab4e7a0e6dd2818ec3ae5218fb78fcfb5ea3a
sunxuemei <641840604@qq.com>
2020-12-11 15:55:13 +0800

    人才招聘更改

--- src/page/develop.vue
+++ src/page/develop.vue
@@ -3,150 +3,138 @@
     <div class="home">
       <Header></Header>
     </div>
-    <div class="banner">
-      <swiper :options="swiperOption" ref="mySwiper">
-
-        <swiper-slide class="slides">
-          <img src="../assets/img/develop/s-bg1.jpg">
-        </swiper-slide>
-        <swiper-slide class="slides">
-          <img src="../assets/img/develop/s-bg2.jpg">
-        </swiper-slide>
-        <!-- <swiper-slide class="slides">
-          <img src="../assets/img/develop/s-bg3.png">
-        </swiper-slide> -->
-        <swiper-slide class="slides">
-          <img src="../assets/img/develop/s-bg4.jpg">
-        </swiper-slide>
-        <swiper-slide class="slides">
-          <img src="../assets/img/develop/s-bg5.jpg">
-        </swiper-slide>
-        <div class="swiper-pagination" slot="pagination"></div>
-
-      </swiper>
-    </div>
+    <div class="banner">    </div>
     <div class="de-inner">
-      <el-tabs type="border-card" v-model="activeName">
-        <el-tab-pane class="nas"  name="first">
-          <span slot="label">外观设计</span>
-          <div class="de-cot">
-            <div class="item">
-              <img src="../assets/img/develop/wai/a1.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/wai/a2.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/wai/a3.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/wai/a4.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/wai/a5.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/wai/a6.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/wai/a7.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/wai/a8.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/wai/a9.jpg"/>
-            </div>
+      <div class="tit1">为什么选择我们</div>
+      <div class="sc-cot">
+      <div class="list">
+        <div class="img">
+          <img src="../assets/img/develop/r1.png"/>
+        </div>
+        <p class="p1">午餐补助</p>   
+        <p class="p2">公司为员工提供免费工作午餐福利，</p>    
+        <p class="p2">按照10元标准固定打到工资卡中。</p> 
+      </div>
+      <div class="list">
+        <div class="img">
+          <img src="../assets/img/develop/r2.png"/>
+        </div>
+        <p class="p1">年度旅游</p>   
+        <p class="p2">公司为了丰富员工生活，实现劳逸</p>    
+        <p class="p2">结合，为员工提供年度旅游活动。</p> 
+      </div>
+      <div class="list">
+        <div class="img">
+          <img src="../assets/img/develop/r3.png"/>
+        </div>
+        <p class="p1">薪资调整</p>   
+        <p class="p2">公司会为满一年入职的员工，提供</p>    
+        <p class="p2">薪资调整的机会。</p> 
+      </div>  
+      <div class="list">
+        <div class="img">
+          <img src="../assets/img/develop/r4.png"/>
+        </div>
+        <p class="p1">培训发展</p>   
+        <p class="p2">公司适时为员工提供培训机会，协</p>    
+        <p class="p2">助员工未来职业发展晋升机会。</p> 
+      </div>          
+      </div>
+      <div class="text">
+        招聘人员：
+        <p>招聘电话：0516-66625073      18912008837</p>                           
+      </div>
+      <div class="box">
+        <div class="b-list">
+          <div class="item">
+            <p>一、急招SMT普工/SMT操机员</p>
+            <p>任职资格：</p>
+            <p>1.招聘员工：10名。年龄18-35岁</p>
+            <p>2.薪资标准：综合月薪4000-6000元</p>
+            <p>3.有无相关工作经验均可，有专职员工带教，上六休一。</p>
           </div>
-        </el-tab-pane>
-        <el-tab-pane >
-          <span slot="label">结构设计</span>
-          <div class="de-cot">
-            <div class="item">
-              <img src="../assets/img/develop/jie/jg1.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/jie/jg2.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/jie/jg3.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/jie/jg4.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/jie/jg5.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/jie/jg6.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/jie/jg7.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/jie/jg8.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/jie/jg9.jpg"/>
-            </div>
+          <div class="item">
+            <p>二、补焊/插件工</p>
+            <p>任职资格：</p>
+            <p>1.招聘员工：15名。年龄18-40岁</p>
+            <p>2.薪资标准：综合月薪3500-4500元</p>
+            <p>3.有无相关工作经验均可，有专职员工带教，上六休一。</p>
           </div>
-        </el-tab-pane>
-        <el-tab-pane >
-          <span slot="label">软件设计</span>
-          <div class="de-cot">
-            <div class="item">
-              <img src="../assets/img/develop/r1.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/r2.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/r3.jpg"/>
-            </div>
+          <div class="item">
+            <p>三、质检员</p>
+            <p>任职资格：</p>
+            <p>1.招聘员工：2名。年龄18-40岁</p>
+            <p>2.薪资标准：综合月薪4500-7000元</p>
+            <p>3.1年以上电子行业质检员工作经验者，上六休一。</p>
+          </div>                    
+        </div>
+        <div class="b-list">
+          <div class="item">
+            <p>四、SMT工程师</p>
+            <p>任职资格：</p>
+            <p>1.招聘员工：5名。年龄18-35岁</p>
+            <p>2.薪资标准：综合月薪6000-8000元</p>
+            <p>3.岗位描述：熟悉SMT工艺流程；</p>
+            <p>熟悉雅马哈系列贴片机的专业维修技能；</p>
+            <p>熟悉SMT的程序优化及新产品导入；</p>
+            <p>熟悉电子元器件的相关知识；</p>
+            <p>4.3年以上工作经验，有专职员工带教，上六休一。</p>
           </div>
-        </el-tab-pane>
-        <el-tab-pane >
-          <span slot="label">硬件设计</span>
-          <div class="de-cot">
-            <div class="item">
-              <img src="../assets/img/develop/ying/yj1.png"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/ying/yj2.png"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/ying/yj3.png"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/ying/yj4.png"/>
-            </div>
-  -->
-            <div class="item">
-              <img src="../assets/img/develop/ying/yj7.png"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/ying/yj8.png"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/ying/yj9.png"/>
-            </div>
+          <div class="item">
+            <p>五、办公文员</p>
+            <p>任职资格：</p>
+            <p>1.招聘员工：1名。年龄18-40岁</p>
+            <p>2.薪资标准：综合月薪3000-3500元</p>
+            <p>3.岗位描述：熟练操作Word、Excel、PPT等办公软件</p>
+            <p>文书档案收集、整理、归档、管理工作；</p>
+            <p>沟通与理解能力强，完成领导突发交代的任务；</p>
+            <p>负责人事、劳资、仓库治理等方面工作；</p>
+            <p>4.2年以上工作经验，单双休。</p>
           </div>
-        </el-tab-pane>
-        <el-tab-pane >
-          <span slot="label">APP设计</span>
-          <div class="de-cot">
-            <div class="item">
-              <img src="../assets/img/develop/r4.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/r5.jpg"/>
-            </div>
-            <div class="item">
-              <img src="../assets/img/develop/r6.jpg"/>
-            </div>
+          <div class="item">
+            <p>六、质量班长/主管</p>
+            <p>任职资格：</p>
+            <p>1.招聘员工：1名。年龄18-40岁</p>
+            <p>2.薪资标准：综合月薪4500-7000元</p>
+            <p>3.1年以上电子行业质量班长/主管工作者，上六休一。</p>
+          </div>                    
+        </div>
+        <div class="b-list">
+          <div class="item">
+            <p>七、项目申报专员</p>
+            <p>任职资格：</p>
+            <p>1.招聘员工：1名。年龄18-40岁</p>
+            <p>2.薪资标准：综合月薪5000-10000元</p>
+            <p>3.岗位描述：熟悉政府项目申报政策和流程的优先考虑；</p>
+            <p>熟悉使用各类办公软件、有一定的财务知识基础；</p>
+            <p>负责客户科技项目申报、人才项目申报、企业资质荣誉、</p>
+            <p>知识产权办理等工作；</p>
+            <p>4.3年以上工作经验，单双休。</p>
           </div>
-        </el-tab-pane>
-      </el-tabs>
+          <div class="item">
+            <p>八、嵌入式软件工程师</p>
+            <p>任职资格：</p>
+            <p>1.招聘员工：5名。年龄18-40岁</p>
+            <p>2.薪资标准：综合月薪6000-20000元</p>
+            <p>3.岗位描述：掌握嵌入式系统分析方法；</p>
+            <p>掌握嵌入式系统设计与开发的方法及步骤；</p>
+            <p>掌握嵌入式系统运行维护知识；</p>
+            <p>了解信息化基础知识、信息技术引用的基础知识；</p>
+            <p>4.3年以上工作经验，单双休。</p>
+          </div>
+          <div class="item">
+            <p>九、嵌入式硬件工程师</p>
+            <p>任职资格：</p>
+            <p>1.招聘员工：10名。年龄18-35岁</p>
+            <p>2.薪资标准：综合月薪6000-20000元</p>
+            <p>3.岗位描述：掌握电子电路原理，模电数电、单片机等知识；</p>
+            <p>熟练使用PCB相关设计软件；</p>
+            <p>负责产品整体设计、电路原理图设计、PCB设计、电路调试</p>
+            <p>具有电子设计、测试、维修等经验；</p>
+            <p>4.3年以上工作经验，单双休。</p>
+          </div>                    
+        </div>                
+        </div>
     </div>
     <div class="home">
       <Footer></Footer>
@@ -158,33 +146,15 @@
 <script>
   import Header from "../components/Header";
   import Footer from "../components/Footer";
-  import { swiper, swiperSlide } from 'vue-awesome-swiper'
   export default {
         name: "develop",
         components:{
             Header,
             Footer,
-            swiper,
-            swiperSlide
         },
         data(){
             return{
-                swiperOption:{
-                    // 参数选项,显示小点
-                    pagination: {
-                        el: '.swiper-pagination',
-                        clickable :true,
-                    },
-                    //循环
-                    loop:true,
-                    //每张播放时长3秒，自动播放
-                    autoplay: true,
-                    //滑动速度
-                    speed:300,
-                    // delay:1000
 
-                },
-                activeName:'first'
             }
         },
     }
@@ -197,77 +167,111 @@
   .banner{
     overflow:hidden;
     width: 100%;
-    height:600px;
-    background :#ccc;
-    .swiper-item{
-      width:100%
-    }
-    .swiper-img{
-      width:100%
-    }
-    .swiper-container {
-               width: 100%;
-             height: 600px;
-         }
+    height:620px;
+    background: url("../assets/img/develop/rcbg.jpg") no-repeat center;
+
   }
 
 .de-inner{
   @include w12;
- .de-cot{
-   overflow: hidden;
-   .item{
-     width: 388px;
-     height: 262px;
-     float: left;
-     margin: 10px 6px;
-   }
- }
-}
-</style>
+  .tit1 {
+    width: 300px;
+    font-size: 36px;
+    letter-spacing: 5px;
+    color: nth($colour, 1);
+    padding: 36px 0 28px 0;
+    box-sizing: border-box;
+    position: relative;
+    margin: 0 auto 40px;
 
-<style lang="scss">
-  .de-inner{
-    .el-tabs--border-card{
-      box-shadow: none;
-      border: none;
-      .el-tabs__header{
-        border: none!important;
-      }
-    }
-    .el-tabs__content{
-      padding: 0!important;
-    }
-    .el-tabs__item{
-      width: 240px!important;
-      text-align: center;
-      /*border: solid 1px #ced1e5!important;*/
-      background: #fff;
-      height: 100px;
-      line-height: 100px;
-      box-sizing: border-box;
-      color: #030303!important;
-      margin: 0!important;
-      float: left!important;
-      font-size: 30px;
-      .nas{
-        i{
-          margin-right: 5px!important;
-        }
-      }
-      &:first-child{
-        margin: 0!important;
-        border-right: none!important;
-      }
-      &:nth-child(2){
-        border-right: none!important;
-      }
-      &:nth-child(3){
-        border-right: none!important;
-      }
-    }
-    .is-active{
-      background-color: $theme-colour!important;
-      color: #fff!important;
+    &:after {
+      display: block;
+      width: 85px;
+      height: 2px;
+      background: nth($colour, 6);
+      content: "";
+      position: absolute;
+      left: 34%;
+      top: 105px;
     }
   }
+        .sc-cot{
+        width: 100%;
+        position: relative;
+        overflow: hidden;
+        padding: 30px 0;
+        box-sizing: border-box;
+        .list{
+          width: 260px;
+          height: 300px;
+          border: solid 2px #e0e0e0;
+          overflow: hidden;
+          float: left;
+          padding: 38px 10px 0 18px;
+    box-sizing: border-box;
+    margin-left: 53px;
+    &:first-child{
+      margin-left: 0!important;
+    }
+          .img{
+            width: 120px;
+            height: 120px;
+            margin: 0 auto;
+          }
+          .p1{
+            font-size: 24px;
+            font-weight: bold;
+            color: #1f5ba3;
+            padding: 15px 0;
+            box-sizing: border-box;
+          }
+          .p2{
+            font-size: 14px;
+            color: #544f4f;
+            line-height: 26px;
+          }
+        }
+      }
+      .text{
+        margin-top: 20px;
+        font-size: 24px;
+         color: #1f5ba3;
+         line-height: 35px;
+         text-align: left;
+         p{
+           display: inline-block;
+               margin-left: 189px;
+         }
+      }
+      .box{
+        overflow: hidden;
+      }
+      .b-list{
+        border-bottom: 2px solid #e0e0e0;
+        padding: 34px 0;
+        box-sizing: border-box;
+        overflow: hidden;
+        .item{
+          float: left;
+          text-align: left;
+          width: 376 px;
+          p{
+          font-size: 14px;
+          color: #544f4f;
+          line-height: 26px;
+          }
+          :first-child{
+            font-size: 18px;
+	          color: #1f5ba3;
+          }
+          :nth-child(2){
+            font-weight: bold;
+          }
+          margin-left: 35px;
+          &:first-child{
+            margin-left: 0!important;
+          }
+        }
+      }
+}
 </style>
